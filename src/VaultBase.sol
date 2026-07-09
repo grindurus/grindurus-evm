f78802b8c052e50dfa6b35298529a716bd3939d1
Pozzitron1337 <the.vaho1337@gmail.com>
2026-07-09 17:11:00 +0300

    merged realization

--- src/VaultBase.sol
+++ src/VaultBase.sol
@@ -7,14 +7,22 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 contract VaultBase {
     using SafeERC20 for IERC20;
 
+    error NotProprietor();
+    error ZeroAddress();
+    error ValueMismatch();
+    error AmountZero();
+    error UnexpectedValue();
+    error ToZero();
+    error EthTransferFailed();
+
     address public immutable PROPRIETOR;
 
     function _onlyProprietor() private view {
-        require(msg.sender == PROPRIETOR, "not proprietor");
+        if (msg.sender != PROPRIETOR) revert NotProprietor();
     }
 
     constructor(address _proprietor) {
-        require(_proprietor != address(0), "zero addr");
+        if (_proprietor == address(0)) revert ZeroAddress();
         PROPRIETOR = _proprietor;
     }
 
@@ -28,10 +36,10 @@ contract VaultBase {
     function deposit(address asset, uint256 amount) public payable {
         _onlyProprietor();
         if (asset == address(0)) {
-            require(msg.value == amount, "value mismatch");
-            require(amount > 0, "amount=0");
+            if (msg.value != amount) revert ValueMismatch();
+            if (amount == 0) revert AmountZero();
         } else {
-            require(msg.value == 0, "unexpected value");
+            if (msg.value != 0) revert UnexpectedValue();
             IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
         }
     }
@@ -39,10 +47,10 @@ contract VaultBase {
     function withdraw(address asset, address to, uint256 amount) public {
         _onlyProprietor();
         if (asset == address(0)) {
-            require(to != address(0), "to=0");
-            require(amount > 0, "amount=0");
+            if (to == address(0)) revert ToZero();
+            if (amount == 0) revert AmountZero();
             (bool ok,) = to.call{value: amount}("");
-            require(ok, "eth transfer failed");
+            if (!ok) revert EthTransferFailed();
         } else {
             IERC20(asset).safeTransfer(to, amount);
         }
