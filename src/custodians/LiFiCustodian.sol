f7f29d76b8f6820c672212094e7cc43a1471a13e
Pozzitron1337 <the.vaho1337@gmail.com>
2026-07-11 06:27:40 +0300

    renamed to custodian

--- src/custodians/LiFiCustodian.sol
+++ src/custodians/LiFiCustodian.sol
@@ -11,7 +11,7 @@ contract LiFiCustodian is Custodian {
         0xd4a33314c9dfc303d37784d8fca3a16a6b60da179d866acaab28e9cd8af2b856; // keccak256("grindurus.custodian.lifi")
 
     /// @inheritdoc Custodian
-    function custodyKind() public pure override returns (bytes32) {
+    function custodianKind() public pure override returns (bytes32) {
         return _CUSTODY_KIND;
     }
 
