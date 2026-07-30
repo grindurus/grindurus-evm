cf2b4e0b84c27f2ffae23af8de80a5e03dd67d1b
Pozzitron1337 <the.vaho1337@gmail.com>
2026-07-30 22:18:23 +0300

    refactored fee variable naming in MockFeeOnTransferERC20 contract for clarity

--- test/mocks/MockFeeOnTransferERC20.sol
+++ test/mocks/MockFeeOnTransferERC20.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @dev Each non-mint/burn transfer delivers `amount * (BPS - feeBps) / BPS` to `to` and burns the fee.
+/// @dev Each non-mint/burn transfer delivers `amount * (BPS - FEE_BPS) / BPS` to `to` and burns the fee.
 contract MockFeeOnTransferERC20 is ERC20 {
     uint8 private immutable _DECIMALS;
-    uint16 public immutable feeBps;
+    uint16 public immutable FEE_BPS;
     uint16 public constant BPS = 10_000;
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_, uint16 feeBps_) ERC20(name_, symbol_) {
         _DECIMALS = decimals_;
-        feeBps = feeBps_;
+        FEE_BPS = feeBps_;
     }
 
     function decimals() public view override returns (uint8) {
@@ -23,8 +23,8 @@ contract MockFeeOnTransferERC20 is ERC20 {
     }
 
     function _update(address from, address to, uint256 value) internal override {
-        if (from != address(0) && to != address(0) && feeBps > 0) {
-            uint256 fee = (value * feeBps) / BPS;
+        if (from != address(0) && to != address(0) && FEE_BPS > 0) {
+            uint256 fee = (value * FEE_BPS) / BPS;
             if (fee > 0) super._update(from, address(0), fee);
             super._update(from, to, value - fee);
             return;
